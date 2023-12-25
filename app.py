9485e37b48829f79a6a9c1b89a484751c81ea1bc
alator-ux <vavilkin.max@mail.ru>
2023-12-25 23:27:22 +0300

    fix (?) app image upload error

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import PIL
+from PIL import Image, ImageOps
 
 uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg", "webp", "tiff"])
 if uploaded_file is not None:
-    image = PIL.Image.open(uploaded_file).convert("RGB")
-    image = PIL.ImageOps.exif_transpose(image)
+    image = Image.open(uploaded_file).convert("RGB")
+    image = ImageOps.exif_transpose(image)
